4042ada41879de7f60c405d7bc3f8522f048d25f
michaeljlin <mjameslin@gmail.com>
2017-09-25 11:31:15 -0700

    Smoother movement

--- prototype/canvasEngine.js
+++ prototype/canvasEngine.js
@@ -4,11 +4,20 @@ var game = null;
 
 var img = new Image();
 
-var playerX = -25;
-var playerY = -25;
+var playerX = 0;
+var playerY = 0;
 
 var canvas = null;
 var ctx = null;
+
+var keys = [];
+
+var faster = 0;
+var slower = 0;
+var baseSpeed = 3;
+var velY = 0;
+var velX = 0;
+var friction = 0.7;
 
 /*
     Main Canvas Setup Function
@@ -18,64 +27,112 @@ function initialize() {
     canvas = document.getElementById('mainScreen');
     ctx = canvas.getContext('2d');
 
-    img.src = "chevron-up.svg";
-
-    draw();
-}
-
-function draw(){
-
-    $('canvas').attr({
-        'width':"600px",
-        'height':"600px"
-    });
-
     ctx.fillStyle = 'white';
+
     ctx.fillRect(0,0,600,600);
 
     ctx.translate(300, 300);
-
 
     img.onload = function(){
         ctx.drawImage(img, playerX, playerY, 50, 50);
     };
 
+    img.src = "chevron-up.svg";
+    $('canvas').attr({
+        'width':"600px",
+        'height':"600px"
+    });
+
+    requestAnimationFrame(draw);
+
+}
+
+function draw(){
+
+    // ctx.fillRect(-300,-300,600,600);
+
+    if(keys[16]){
+        faster = 2;
+        friction = 0.85;
+    }
+    else{
+        faster = 0;
+        friction = 0.7;
+    }
+
+    if(keys[88]){
+        slower = -2;
+    }
+    else{
+        slower = 0;
+    }
+
+    if(keys[38]){
+        playerY-=baseSpeed+faster+slower;
+    }
+
+    if(keys[40]){
+        playerY+=baseSpeed+faster+slower;
+    }
+
+    if(keys[39]){
+        playerX+=baseSpeed+faster+slower
+    }
+
+    if(keys[37]){
+        playerX-=baseSpeed+faster+slower;
+    }
+
+    playerY *= friction;
+    velY += playerY;
+
+    playerX *= friction;
+    velX += playerX;
+
+    ctx.clearRect(0,0, 600, 600);
+
+    ctx.fillStyle = 'white';
+
+    ctx.fillRect(0,0,600,600);
+
+    ctx.drawImage(img, velX, velY, 50, 50);
+
     game = ctx;
+
+    requestAnimationFrame(draw);
 }
 
 /*
     Input Functions
  */
 
-$(document).on("keydown", function(event){
-    var key = event.key;
-    console.log("Pressed: "+key);
+// $(document).on("keydown", function(event){
+//     var key = event.key;
+//     console.log("Pressed: "+key);
+//
+//     // switch(key){
+//     //     case 'ArrowUp':
+//     //         playerY-=3;
+//     //         break;
+//     //     case 'ArrowDown':
+//     //         playerY+=3;
+//     //         break;
+//     //     case 'ArrowLeft':
+//     //         playerX-=3;
+//     //         break;
+//     //     case 'ArrowRight':
+//     //         playerX+=3;
+//     //         break;
+//     //     default:
+//     // }
+//
+//
+// });
 
-    switch(key){
-        case 'ArrowUp':
-            game.clearRect(-300,-300,canvas.width, canvas.height);
-            ctx.fillRect(-300,-300,600,600);
-            playerY-=5;
-            ctx.drawImage(img, playerX, playerY, 50, 50);
-            break;
-        case 'ArrowDown':
-            game.clearRect(-300,-300,canvas.width, canvas.height);
-            ctx.fillRect(-300,-300,600,600);
-            playerY+=5;
-            ctx.drawImage(img, playerX, playerY, 50, 50);
-            break;
-        case 'ArrowLeft':
-            game.clearRect(-300,-300,canvas.width, canvas.height);
-            ctx.fillRect(-300,-300,600,600);
-            playerX-=5;
-            ctx.drawImage(img, playerX, playerY, 50, 50);
-            break;
-        case 'ArrowRight':
-            game.clearRect(-300,-300,canvas.width, canvas.height);
-            ctx.fillRect(-300,-300,600,600);
-            playerX+=5;
-            ctx.drawImage(img, playerX, playerY, 50, 50);
-            break;
-        default:
-    }
+$(document).on("keydown", function (e) {
+    keys[e.keyCode] = true;
+});
+
+$(document).on("keyup", function (e) {
+    keys[e.keyCode] = false;
 });
